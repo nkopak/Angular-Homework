33ffc17f12dd9037b35bfacb9dbfc585d68714b5
nazariikopak <123nazik@gmail.com>
2021-01-21 15:58:02 +0200

    ang hw 2

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,23 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Post} from '../../../models/Post';
+
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  @Input() post: Post;
+
+  @Output() bubbleUp = new EventEmitter<Post>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getPostInfo(post: Post): void{
+    this.bubbleUp.emit(post);
+  }
+
+}
---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,27 @@
+import {HttpClient} from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {Post} from '../../../models/Post';
+import {PostService} from '../../services/post.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  posts: Post[];
+  singlePost: Post;
+
+  constructor(private postService: PostService) {
+  }
+
+  ngOnInit(): void {
+  this.postService.getPosts().subscribe(value => this.posts = value);
+  }
+
+  getBubble(post: Post): void {
+    this.singlePost = post;
+  }
+
+}
