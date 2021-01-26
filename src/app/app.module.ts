e48772cbaaa03feb6b0fb52abcf48d17b7947781
nazariikopak <123nazik@gmail.com>
2021-01-26 18:02:18 +0200

    ang_lesson5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,22 @@ import { UsersComponent } from './components/users/users.component';
 import {RouterModule, Routes} from '@angular/router';
 import {UserResolveService} from './services/user-resolve.service';
 import { UserComponent } from './components/users/user/user.component';
+import { FullUserComponent } from './components/users/full-user/full-user.component';
 
-const routes: Routes = [{
-  path: '', component: UsersComponent, resolve: {userData: UserResolveService}
-}];
+const routes: Routes = [
+  {path: 'users', component: UsersComponent, resolve: {userData: UserResolveService},
+  children: [
+    {path: ':id', component: FullUserComponent}
+  ]},
+  {path: '', redirectTo: 'users', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     UserComponent,
+    FullUserComponent,
   ],
   imports: [
     BrowserModule,
