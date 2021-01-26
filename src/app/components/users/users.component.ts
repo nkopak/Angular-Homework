9d64f549ba0187a1ea3bdd30230b92c6451b05c7
nazariikopak <123nazik@gmail.com>
2021-01-26 14:38:58 +0200

    ang_lesson5

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../../services/user/user.service';
+import {User} from '../../models/user';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  users: User[];
+
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.data.subscribe(value => this.users = value.userData);
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
