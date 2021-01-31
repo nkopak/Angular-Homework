4fbafffdac113e7163ecf7b4520ce8430c6d0e78
nazariikopak <123nazik@gmail.com>
2021-01-31 02:23:15 +0200

    ang lesson1 hw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CarsComponent } from './cars/cars.component';
+import { CarComponent } from './cars/car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CarsComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/data/cars.ts
@@ -0,0 +1,7 @@
+export const cars = [
+  {brand: 'Skoda', model: 'Fabia', engineVolume: 1400, prodYear: 2008},
+  {brand: 'ZAZ', model: 'Tavria', engineVolume: 1100, prodYear: 1992},
+  {brand: 'Mazda', model: 'CX-5', engineVolume: 2500, prodYear: 2015},
+  {brand: 'Honda', model: 'CR-V', engineVolume: 2200, prodYear: 2010},
+  {brand: 'Renault', model: 'Megane', engineVolume: 1500, prodYear: 2010}
+];
