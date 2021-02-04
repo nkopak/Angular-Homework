f811434aac66e082f1f32e8b7a6cee6831b9b0ca
nazariikopak <123nazik@gmail.com>
2021-02-04 18:01:45 +0200

    ang lesson 4 hw routes 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,24 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import { UsersComponent } from './components/users/users.component';
+import {UsersComponent} from './components/users/users.component';
 import {RouterModule} from '@angular/router';
 import {UserResolveService} from './services/resolve/user-resolve.service';
-import { UserComponent } from './components/users/user/user.component';
+import {UserComponent} from './components/users/user/user.component';
+import {FullUserComponent} from './components/users/full-user/full-user.component';
 
-const routes = [{
-  path: '', component: UsersComponent, resolve: {usersData: UserResolveService}
-}];
+const routes = [
+  {
+    path: 'users', component: UsersComponent, resolve: {usersData: UserResolveService},
+    children: [
+      {path: ':id', component: FullUserComponent}
+    ]
+  },
+  {path: '', redirectTo: 'users', pathMatch: 'full'}
+];
 
 @NgModule({
-  declarations: [AppComponent, UsersComponent, UserComponent],
+  declarations: [AppComponent, UsersComponent, UserComponent, FullUserComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
