2358d577e5cf31c21c030cd8c59fc84c5ef3a3c8
nazariikopak <123nazik@gmail.com>
2021-02-09 17:55:21 +0200

    ang lesson 6 forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Homework';
+  user = {name: 'yura', age: 123};
+
+  name = new FormControl( '', [
+    Validators.required,
+    // Validators.minLength(10),
+    this.customValidator
+  ]);
+  age = new FormControl();
+
+  myForm = new FormGroup({
+    name: this.name,
+    age: this.age
+  });
+
+  // submit(form: NgForm): void{
+  //   console.log(form);
+  // }
+
+  submit(myForm: FormGroup): void{
+    console.log(myForm);
+  }
+
+  customValidator(inputData: AbstractControl): any {
+    if(inputData.value === 'fuck'){
+      return {error: true, msg: '123'};
+    } else {
+      return null
+    }
+  }
 }
